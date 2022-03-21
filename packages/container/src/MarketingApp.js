4939d59a076ef33eeea0794e24eb7c7c4cdf56d5
Haidukov <rostyk.haidukov1999@gmail.com>
2022-03-21 22:54:15 +0200

    add fix routing

--- packages/container/src/MarketingApp.js
+++ packages/container/src/MarketingApp.js
@@ -1,11 +1,23 @@
 import React, { useEffect, useRef } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import { mount } from 'marketing/MarketingApp';
 
 const MarketingApp = () => {
     const marketingRef = useRef(null);
+    const history = useHistory();
+    const location = useLocation();
     useEffect(() => {
-        mount(marketingRef.current);
-    }, [marketingRef]);
+        const { onParentNavigate } = mount(marketingRef.current, {
+            initialPathname: location.pathname,
+            onNavigate: ({ pathname: nextPathname }) => {
+                if (location.pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            }
+        });
+        const unsubscribe = history.listen(onParentNavigate);
+        return () => unsubscribe();
+    }, []);
     return <div ref={marketingRef} />
 };
 
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 import App from './App';
 
-const mount = (el) => {
-    ReactDOM.render(<App />, el)
+const mount = (el, { onNavigate, defaultHistory, initialPathname } = {}) => {
+    const history = defaultHistory || createMemoryHistory();
+    if (history.location.pathname !== initialPathname) {
+        history.push(initialPathname);
+    }
+
+    const onParentNavigate = ({ pathname: nextPathname }) => {
+        if (history.location.pathname !== nextPathname) {
+            history.push(nextPathname);
+        }
+    };
+
+    console.log(onNavigate);
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+    ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate
+    }
 };
 
 if (process.env.NODE_ENV === 'development') {
     const devEl = document.getElementById('marketing-dev');
     if (devEl) {
-        mount(devEl);
+        mount(devEl, {
+            defaultHistory: createBrowserHistory()
+        });
     }
 }
 
